363a8c5e71615571ee06ba1baa9f34d02349ab15
Your Name <b122310422@student.utem.edu.my>
2026-09-01 14:31:12 +0800

    change homepage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,18 +4,12 @@ import './LandingPage.css';
 export default function LandingPage({ onGoToLogin }) {
   return (
     <div className="embed-landing-container">
-      {/* Poster Rekaan */}
-      <img 
-        src="/Homepage.png" 
-        alt="Proton ME Data Tracking System" 
-        className="poster-image" 
-      />
-
       {/* Butang Lutsinar bertindih di atas butang LOGIN poster */}
       <button 
         className="invisible-login-btn" 
         onClick={onGoToLogin}
         title="Click to Log In"
+        aria-label="Log In"
       >
         LOGIN
       </button>
